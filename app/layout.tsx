4f3159d58a640e0fd46eda44b31965a20e8538c7
parsanojavan <parsa.nojavan85@gmail.com>
2026-07-07 02:07:40 +0330

    refactor(app): move routes into main and editor groups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import TopBar from "@/components/TopBar";
-import SideBar from "@/components/SideBar";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
-import TitleBar from "@/components/TitleBar";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({
   children,
@@ -15,21 +11,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn("dark", "font-sans", geist.variable)}>
-      <body className="bg-zinc-950 text-white flex flex-col h-screen w-screen overflow-hidden select-none">
-
-        <TitleBar/>
-
-        <TopBar />
-
-        <div className="flex flex-1 overflow-hidden">
-          
-          <SideBar />
-
-          <main className="flex-1 overflow-y-auto bg-zinc-950 p-8">
-            {children}
-          </main>
-          
-        </div>
+      <body className="bg-zinc-950 text-white flex h-screen w-screen overflow-hidden select-none">
+        {children}
       </body>
     </html>
   );
